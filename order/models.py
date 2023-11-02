e19e35e01fb0295f76816d100f2beca4c705de7f
htsaii <htsai2@stevens.edu>
2023-11-02 02:55:58 -0400

    add the dropdown
    * dropdown to select filter the company
    based on the date of the week.
    * add the item that company is will order.

--- order/models.py
+++ order/models.py
@@ -3,9 +3,14 @@ from order import db
 
 
 class Order(db.Model):
+    """
+    # self.customer :
+    # self.items : 
+    """
     id = db.Column(db.Integer, primary_key=True)
-    customer_name = db.Column(db.String(200), nullable=True)
-    order_items = db.Column(db.String(200), nullable=True)
-    order_date = db.Column(db.String(200), nullable=False, default=datetime.utcnow().strftime('%Y-%m-%d'))
+    customer = db.Column(db.String(200), nullable=True)
+    
+    items = db.Column(db.String(200), nullable=True)
+    date = db.Column(db.String(200), nullable=False, default=datetime.utcnow().strftime('%Y-%m-%d'))
     # order_date = db.Column(db.DateTime, default=datetime.utcnow)
     
--- order/routes.py
+++ order/routes.py
@@ -2,6 +2,31 @@ from flask import render_template, url_for, request, redirect, session
 from datetime import datetime
 from order.models import Order
 from order import app, db
+from order.services import *
+
+
+"""
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    # print(request.method)
+    if request.method == 'POST':
+        # new_order = Order(customer_name=request.form['customer_name'], order_items=request.form['order_items'], order_date=request.form['order_date'])
+        customer = request.form['customer_name'] 
+        items = request.form['order_items']
+        date = request.form['order_date'] if request.form['order_date'] else datetime.utcnow().strftime('%Y-%m-%d')
+        
+        orders = order_precess(items)
+
+        try:
+            for order in orders:
+                pass
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return 'There was an issue adding your order'
+    else:
+        orders = Order.query.order_by(Order.date).all()
+        return render_template('index.html', orders = orders)
+"""
 
 
 @app.route('/', methods = ['POST', 'GET'])
@@ -9,12 +34,15 @@ def index():
     # print(request.method)
     if request.method == 'POST':
         # new_order = Order(customer_name=request.form['customer_name'], order_items=request.form['order_items'], order_date=request.form['order_date'])
-        customer_name = request.form['customer_name'] 
-        order_items = request.form['order_items']     
-        order_date = request.form['order_date'] if request.form['order_date'] else datetime.utcnow().strftime('%Y-%m-%d')
+        customer = request.form['customer_name'] 
+        items = request.form['order_items']
+        date = request.form['order_date'] if request.form['order_date'] else datetime.utcnow().strftime('%Y-%m-%d')
         
-        new_order = Order(customer_name=customer_name, order_items=order_items, order_date=order_date)
-        print(new_order.customer_name, new_order.order_items, new_order.order_date)
+
+
+        new_order = Order(customer=customer, items=items, date=date)
+        print(new_order.customer, new_order.items, new_order.date)
+        
         try:
             db.session.add(new_order)
             db.session.commit()
@@ -23,7 +51,7 @@ def index():
             print(f"An error occurred: {e}")
             return 'There was an issue adding your order'
     else:
-        orders = Order.query.order_by(Order.order_date).all()
+        orders = Order.query.order_by(Order.date).all()
         return render_template('index.html', orders = orders)
     
 @app.route('/delete/<int:id>')
@@ -42,10 +70,10 @@ def update(id):
     order = Order.query.get_or_404(id)
 
     if request.method == 'POST':
-        order.customer_name = request.form['customer_name']
-        order.order_items = request.form['order_items']
+        order.customer = request.form['customer_name']
+        order.items = request.form['order_items']
         print(request.form['order_date'])
-        order.order_date = request.form['order_date']
+        order.date = request.form['order_date']
 
         try:
             db.session.commit()
@@ -62,9 +90,9 @@ def search():
     
     q = request.form.get('search_query')
     if q:
-        orders = Order.query.filter(Order.customer_name.icontains(q)).order_by(Order.order_date).all()
+        orders = Order.query.filter(Order.customer.icontains(q)).order_by(Order.date).all()
     else:
-        orders = Order.query.order_by(Order.order_date).all()
+        orders = Order.query.order_by(Order.date).all()
 
     return render_template('search.html', orders = orders)
         
